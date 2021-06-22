14f7380a7808ecf839b68eac27198b612506c790
chenghaojason <whx20100101@163.com>
2021-06-22 14:08:12 +0800

    图的邻接矩阵存储

---
+++ dsaa-dataStructure/src/main/java/com/jason/structure/nonlinearity/figure/Graph.java
@@ -0,0 +1,6 @@
+package com.jason.structure.nonlinearity.figure;/**
+ * @author WangChenHol
+ * @date 2021/6/18 14:25
+ **/
+public interface Graph {
+}
---
+++ dsaa-dataStructure/src/test/java/com/jason/structure/linearity/nonlinearity/TestGraph.java
@@ -0,0 +1,6 @@
+package com.jason.structure.linearity.nonlinearity;/**
+ * @author WangChenHol
+ * @date 2021/6/21 14:37
+ **/
+public class TestGraph {
+}
